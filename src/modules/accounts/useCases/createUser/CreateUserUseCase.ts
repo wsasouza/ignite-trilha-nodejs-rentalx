b2b860b27a1f04a8c8531c1da60d05cd4f011c75
wsasouza <wsasouza@hotmail.com>
2021-06-17 07:21:12 -0300

    Encrypt password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
-
+import { hash } from 'bcrypt';
 @injectable()
 class CreateUserUseCase {
 
@@ -16,9 +16,17 @@ class CreateUserUseCase {
       email, 
       driver_license
   }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
     await this.usersRepository.create({
       name,      
-      password,
+      password: passwordHash,
       email,
       driver_license
     });
