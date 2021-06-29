3d8de2432c34a4b2d946f50d2d5e5a4bb24ac466
wsasouza <wsasouza@hotmail.com>
2021-06-29 06:53:30 -0300

    Authentication on routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import { ensureAuthenticate } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListSpecificationsController } from '../modules/cars/useCases/listSpecifications/ListSpecificationsController';
 
 const specificationsRoutes = Router();
+
+specificationsRoutes.use(ensureAuthenticate);
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
