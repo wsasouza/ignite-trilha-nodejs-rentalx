d0aa9107226d3a85284197c19c26adcff4306300
wsasouza <wsasouza@hotmail.com>
2022-01-31 14:07:32 -0300

    Feat: Create Category Test

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,38 @@
 import "reflect-metadata";
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
-import swaggerUi from 'swagger-ui-express';
-import swaggerFile from './swagger.json';
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
-import './database';
+import "./database";
 
-import './shared/container';
+import "./shared/container";
 
-import { router } from './routes';
-import { AppError } from './errors/AppError';
+import { router } from "./routes";
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile) );
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message
-    })
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
   }
+);
 
-  return response.status(500).json({
-    status: "error",
-    message: `Internal server error - ${err.message}`
-  });
-});
-
-app.listen(3333, () => console.log('Server is running!'));
+app.listen(3333, () => console.log("Server is running on port 3333."));
